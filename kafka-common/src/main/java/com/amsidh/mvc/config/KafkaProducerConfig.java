8dd2f5c12f3c1373423e2ee3749d0ab914aaa04d
amsidh.lokhande@xebia.com <amsidh.lokhande@xebia.com>
2022-09-09 15:40:19 +0530

    Use of KafkaProducer to send the message

--- kafka-common/src/main/java/com/amsidh/mvc/config/KafkaProducerConfig.java
+++ kafka-common/src/main/java/com/amsidh/mvc/config/KafkaProducerConfig.java
@@ -1,8 +1,7 @@
 package com.amsidh.mvc.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,24 +12,20 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Configuration
 public class KafkaProducerConfig {
-	
+
 	@Value("${spring.kafka.producer.bootstrap-servers:localhost:29092,localhost:39092}")
 	private String bootstrapServerConfig;
 
 	@Bean
 	public ProducerFactory<String, Object> getProducerFactory() {
 		log.debug("Creating bean of ProducerFactory!!!!");
-		Map<String, Object> kafkaProducerConfigMap = new HashMap<>();
-		kafkaProducerConfigMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
-		kafkaProducerConfigMap.put(ProducerConfig.ACKS_CONFIG, "all");
-
-		kafkaProducerConfigMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		kafkaProducerConfigMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		Map<String, Object> kafkaProducerConfigMap = getKafkaProducerConfigMap();
 		return new DefaultKafkaProducerFactory<>(kafkaProducerConfigMap);
 
 	}
@@ -41,4 +36,21 @@ public class KafkaProducerConfig {
 		KafkaTemplate kafkaTemplate = new KafkaTemplate(getProducerFactory());
 		return kafkaTemplate;
 	}
+
+	@Bean
+	public KafkaProducer<String, Object> getKafkaProducer() {
+		return new KafkaProducer(getKafkaProducerConfigMap());
+	}
+
+
+	private Map<String, Object> getKafkaProducerConfigMap() {
+		Map<String, Object> kafkaProducerConfigMap = new HashMap<>();
+		kafkaProducerConfigMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerConfig);
+		kafkaProducerConfigMap.put(ProducerConfig.ACKS_CONFIG, "all");
+
+		kafkaProducerConfigMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		kafkaProducerConfigMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		return kafkaProducerConfigMap;
+	}
+
 }
--- kafka-producer/src/main/java/com/amsidh/mvc/controller/MessagePublishController.java
+++ kafka-producer/src/main/java/com/amsidh/mvc/controller/MessagePublishController.java
@@ -3,6 +3,7 @@ package com.amsidh.mvc.controller;
 import com.amsidh.mvc.model.PersonRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -18,6 +19,7 @@ import java.util.UUID;
 public class MessagePublishController {
 
 	private final KafkaTemplate<String, Object> kafkaTemplate;
+	private final KafkaProducer<String, Object> kafkaProducer;
 	private final static String KAFKA_TOPIC_NAME = "spring-topic";
 
 	@GetMapping("/publish/{name}")
@@ -42,8 +44,15 @@ public class MessagePublishController {
 
 		ProducerRecord<String, Object> producerRecord = new ProducerRecord<>(KAFKA_TOPIC_NAME, personRequest.getUuid(), personRequest);
 
-		ListenableFuture<SendResult<String, Object>> sendResultListenableFuture = kafkaTemplate.send(producerRecord);
-		sendResultListenableFuture.addCallback(result -> log.info("Message published successfully {}", result.getProducerRecord().value().toString()), ex -> log.error("Failed to publish message", ex));
+		//ListenableFuture<SendResult<String, Object>> sendResultListenableFuture = kafkaTemplate.send(producerRecord);
+		kafkaProducer.send(producerRecord, (metadata, exception) -> {
+			if (exception != null) {
+				log.error("Failed to publish message", exception);
+			}
+			if (metadata != null) {
+				log.info("Message published successfully {}", personRequest);
+			}
+		});
 		return "Message published successfully";
 	}
 
